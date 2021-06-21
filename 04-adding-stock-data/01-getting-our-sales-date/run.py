bdb3b669f6b1ca5a49e3748f220d7751965ef83c
Anna Greaves <gilhespy.anna@gmail.com>
2021-06-21 10:02:44 +0200

    Add missing sourcecode to getting sales data lesson.

--- 04-adding-stock-data/01-getting-our-sales-date/run.py
+++ 04-adding-stock-data/01-getting-our-sales-date/run.py
@@ -87,6 +87,22 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns
+
+
 def main():
     """
     Run all program functions
@@ -99,4 +115,6 @@ def main():
 
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+# main()
+
+sales_columns = get_last_5_entries_sales()
